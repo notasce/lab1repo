3f03b1e366c1f4b04c48773f6c2bb4f2133b617f
asce15 <nashdave15@gmail.com>
2025-09-22 15:32:18 +0800

    sir here is another one to be checked

---
+++ decision.php
@@ -0,0 +1,13 @@
+<?php
+// Hardcoded grade value
+$grade = 92;
+
+// Check conditions using if...else
+if ($grade >= 90) {
+    echo "With Honors.";
+} elseif ($grade >= 75) {
+    echo "Passed";
+} else {
+    echo "Failed";
+}
+?>
